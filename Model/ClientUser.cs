92158c53b84f84e4bf03c39f0ef62334452d1d09
Kapil Patel <kapilepatel@gmail.com>
2019-07-03 13:50:08 +0530

    initil commit

---
+++ Model/ClientUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AG_MS_Authentication.Model
+{
+    public class ClientUser
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+
+namespace MS_Authentication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //configure log4net file
+            //add file log4net.config.xml in each microservice api project
+            var logRepo = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepo, new FileInfo("log4net.config.xml"));
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(config =>
+                {
+                    config.AddJsonFile("appsettings.json");
+                    config.AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>();
+    }
+}
